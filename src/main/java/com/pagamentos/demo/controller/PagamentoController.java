13604859514300d097216a430e7d5f4e9512e341
patricchubaca <patric_job@outlook.com>
2025-02-26 12:08:14 -0300

    Feat :: Hellow Word

--- src/main/java/com/pagamentos/demo/controller/PagamentoController.java
+++ src/main/java/com/pagamentos/demo/controller/PagamentoController.java
@@ -1,5 +1,29 @@
 package com.pagamentos.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pagamentos.demo.model.Pagamento;
+import com.pagamentos.demo.services.PagamentoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/pagamentos")
 public class PagamentoController {
+
+    @Autowired
+    private PagamentoService service;
+
+    @GetMapping("/")
+    public String getHellow() {
+        return "Hellow";
+    }
+
+    @GetMapping("/d")
+    public String getMethodName(@RequestParam String param) {
+        return new String("Hellow");
+    }
 
 }
